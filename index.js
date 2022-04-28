fb81037ef6f416cba022c134e763059981789c73
Ruman-Islam <rumanislam48@gmail.com>
2022-04-29 02:25:20 +0600

    server started

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running well')
+})
+
+app.listen(port, () => {
+    console.log('Dream Weaver server is running on port -', port);
+})
