964663cb6ad59626d38053103842444eb7f379dd
Ruman-Islam <rumanislam48@gmail.com>
2022-05-02 08:49:01 +0600

    delete api added

--- index.js
+++ index.js
@@ -19,11 +19,25 @@ const run = async () => {
     try {
         await client.connect();
         const productsCollection = client.db("warehouse").collection("products");
+        const sellersInfoCollection = client.db("warehouse").collection("sellers-info");
+        const topSellingProductsCollection = client.db("warehouse").collection("top-selling-products");
 
         // http://localhost:5000/products
         app.get('/products', async (req, res) => {
             const query = req.query;
             const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            if (!products) {
+                res.send({ success: false, message: "Products not available right now" })
+            } else {
+                res.send({ success: true, products })
+            }
+        })
+
+        // http://localhost:5000/products/user
+        app.get('/products-user', async (req, res) => {
+            const email = req.query.email;
+            const cursor = productsCollection.find({ email });
             const products = await cursor.toArray();
             if (!products) {
                 res.send({ success: false, message: "Products not available right now" })
@@ -48,20 +62,60 @@ const run = async () => {
         app.put('/product/:productId', async (req, res) => {
             const productId = req.params.productId;
             const updateQuantity = req.body;
-            const filter = { _id: ObjectId(productId) };
+            const query = { _id: ObjectId(productId) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
                     quantity: updateQuantity.newQuantity
                 }
             }
-            const result = await productsCollection.updateOne(filter, updatedDoc, options);
+            const result = await productsCollection.updateOne(query, updatedDoc, options);
             if (!result) {
                 res.send({ success: false, message: "Sorry! couldn't update this time" })
             } else {
                 res.send({ success: true, result })
             }
         })
+
+        // http://localhost:5000/add-product
+        app.post('/add-product', async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            if (!result) {
+                res.send({ success: false, message: "Sorry! couldn't add this time" })
+            } else {
+                res.send({ success: true, result })
+            }
+
+        })
+
+        // http://localhost:5000/top-sellers
+        app.get('/top-sellers', async (req, res) => {
+            const query = req.query;
+            const cursor = sellersInfoCollection.find(query);
+            const sellersInfo = await cursor.toArray();
+            res.send(sellersInfo);
+        })
+
+        // http://localhost:5000/top-selling-products
+        app.get('/top-selling-products', async (req, res) => {
+            const query = req.query;
+            const cursor = topSellingProductsCollection.find(query);
+            const topSellingProducts = await cursor.toArray();
+            res.send(topSellingProducts);
+        })
+
+        app.delete('/delete-product/:productId', async (req, res) => {
+            const productId = req.params.productId;
+            const query = { _id: ObjectId(productId) };
+            const product = await productsCollection.deleteOne(query);
+            if (!product) {
+                res.send({ success: false, message: "Sorry! couldn't delete this time" })
+            } else {
+                res.send({ success: true, product })
+            }
+        })
+
     } finally {
         // await client.close();
     }
